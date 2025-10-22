acb210acb0b77ffae6b0a709d5a3e97f88748d1d
Myriam <m.zinco@studenti.unibs.it>
2025-10-22 22:17:18 +0200

    modifiche di sensore t interna

--- src/it/unibs/VAMsmarthouse/SensoreTInterna.java
+++ src/it/unibs/VAMsmarthouse/SensoreTInterna.java
@@ -1,19 +1,27 @@
 package it.unibs.VAMsmarthouse;
 
-public class SensoreTInterna extends Sensore<Double> {
+import java.util.Random;
 
+public class SensoreTInterna extends Sensore<Double> {
+	private static final long COMMON_SEED = 1234L;
+	private final Random random = new Random(COMMON_SEED);
+	private final double offset;
 	private SensoreTEsterna esterna;
 
-	public SensoreTInterna(int id, SensoreTEsterna esterna) {
+	public SensoreTInterna(String id, SensoreTEsterna esterna, double offset) {
 		super(id, 500, "Temperatura interna");
 		this.esterna = esterna;
-
+		switch (id) {
+		case "t-0" -> this.offset = 0.5;
+		case "t-1" -> this.offset = 1.5;
+		default -> this.offset = 0.0;
+		}
 	}
 
 	@Override
 	protected Double generaValore()// i metodi astratti si overridano tutti
 	{
-		return esterna.getValue() + (-5 + Math.random() * 10);
+		return esterna.getValue() + (-5 + random.nextDouble() * 10) + offset;
 	}
 
 }// sicuramente poi dovremo sistemare il fatto che se ho due sensori interni che
