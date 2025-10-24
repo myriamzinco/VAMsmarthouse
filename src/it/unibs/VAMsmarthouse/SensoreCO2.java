fee672edd3526fbab259516f938d5824eb4f80af
vanessasavoldi <v.savoldi006@studenti.unibs.it>
2025-10-24 09:40:40 +0200

    Sensore movimento

--- src/it/unibs/VAMsmarthouse/SensoreCO2.java
+++ src/it/unibs/VAMsmarthouse/SensoreCO2.java
@@ -1,5 +1,14 @@
 package it.unibs.VAMsmarthouse;
 
-public class SensoreCO2 {
+public class SensoreCO2 extends Sensore<Double>{
 
+	public SensoreCO2(int id, long time, String type) {
+		super(id, time, "Humidity");
+	}
+
+	@Override
+	protected Double generaValore() {
+		return -5 + (40 * Math.random());
+	}
+	
 }
--- src/it/unibs/VAMsmarthouse/SensoreMovimento.java
+++ src/it/unibs/VAMsmarthouse/SensoreMovimento.java
@@ -1,5 +1,16 @@
 package it.unibs.VAMsmarthouse;
 
-public class SensoreMovimento {
+public class SensoreMovimento extends Sensore<Boolean>{
 
+	public SensoreMovimento(int id, long time, String type) {
+		super(id, time, "Movimento");
+	}
+
+	@Override
+	protected Boolean generaValore() {
+	    int num = (int) (Math.random() * 2);
+	    return num == 1; // true se num = 1
+	}
+
+	
 }
